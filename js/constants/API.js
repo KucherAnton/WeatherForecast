b35a226dbd698f214f70ee351c018c44434fb14c
KucherAnton <kapetrovi4@gmail.com>
2022-01-06 19:35:56 +0300

    forget about this, sry

--- js/constants/API.js
+++ js/constants/API.js
@@ -1,6 +1,14 @@
 export let weather = {
-    'apiKey': '10d94c1a9d6f2130fc13aa42cfcebb00'
-    fetchWeather: function () {
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric&appid=10d94c1a9d6f2130fc13aa42cfcebb00').then((response) => response.json()).then((data) => console.log(data))
+    apiKey: '10d94c1a9d6f2130fc13aa42cfcebb00',
+    fetchWeather: function (city) {
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=' 
+        + city 
+        + '&units=metric&appid=' 
+        + this.apiKey)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    },
+    displayWeather: function(data) {
+        
     }
-}
+};
