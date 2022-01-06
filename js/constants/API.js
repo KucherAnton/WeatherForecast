9dbe809f9a555a5f50fcfbc6d32b6c448c49df2d
KucherAnton <kapetrovi4@gmail.com>
2022-01-06 19:22:09 +0300

    weatherInfo 1 part

--- js/constants/API.js
+++ js/constants/API.js
@@ -1,1 +1,6 @@
-export const XUI = 'pasdasodao';
+export let weather = {
+    'apiKey': '10d94c1a9d6f2130fc13aa42cfcebb00'
+    fetchWeather: function () {
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric&appid=10d94c1a9d6f2130fc13aa42cfcebb00').then((response) => response.json()).then((data) => console.log(data))
+    }
+}
