de90c1bd53c48a19566a8c6545efe0dd6bb0354a
KucherAnton <kapetrovi4@gmail.com>
2022-01-06 20:42:26 +0300

    weatherInfo part 2

--- js/constants/API.js
+++ js/constants/API.js
@@ -1,4 +1,4 @@
-export let weather = {
+let weather = {
     apiKey: '10d94c1a9d6f2130fc13aa42cfcebb00',
     fetchWeather: function (city) {
         fetch('https://api.openweathermap.org/data/2.5/weather?q=' 
@@ -6,9 +6,21 @@ export let weather = {
         + '&units=metric&appid=' 
         + this.apiKey)
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => this.displayWeather(data));
+        console.log('Проверка')
     },
     displayWeather: function(data) {
-        
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.speed;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector('.city').innerText = 'Weather in' + city;
+        document.querySelector('.search-icon').src = 'https://openweathermap.org/img/wn/' + icon + '.png';
+        document.querySelector('.description__weather').innerText = description;
+        document.querySelector('.temp').innerText = temp + '°C';
+        document.querySelector('.humidity').innerText = 'Humidity: ' + humidity + '%';
+        document.querySelector('.wind').innerText = 'Wind speed: ' + speed + ' km/h';
+        console.log('Проверка')
     }
 };
